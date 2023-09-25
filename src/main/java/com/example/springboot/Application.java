bd1bed97d9cccf45096169412944bb99dffb1952
Abhishek Singh <abhishek.abhi90@gmail.com>
2023-09-25 16:47:06 +0530

    Changes pushed

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -12,7 +12,6 @@ import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
